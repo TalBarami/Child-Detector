57ba714c2c81732f5eb34786ccc699712749bb79
Tal Baramit <baramit@post.bgu.ac.il>
2022-07-07 09:22:26 +0300

    - Setup.py generated.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Child-Detector',
+    version='1.0',
+    packages=['child_detector'],
+    url='https://github.com/TalBarami/Child-Detector',
+    license='',
+    author='TalBarami',
+    author_email='baramit@post.bgu.ac.il',
+    description='Model for child-adult detection.'
+)
